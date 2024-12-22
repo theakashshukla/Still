c6b887a6ca1f3029f14d085365b8617f824ce63f
Akash Shukla <49320497+theakashshukla@users.noreply.github.com>
2024-12-22 12:23:31 +0530

    feat: Enhance ToolCard layout and add new Cypher Wallet product details

--- components/tool-card.tsx
+++ components/tool-card.tsx
@@ -29,7 +29,7 @@ export function ToolCard({ searchQuery }: ToolCardProps) {
 
   return (
     <>
-      <div className="flex space-x-4 mb-10 justify-center">
+      <div className="flex space-x-4 space-y-2 mb-10 justify-center flex-wrap">
         <button
           onClick={() => setSelectedCategory(null)}
           className={`px-4 py-2 rounded-full ${
@@ -54,7 +54,7 @@ export function ToolCard({ searchQuery }: ToolCardProps) {
           </button>
         ))}
       </div>
-      <section className="p-4 mx-14 min-h-screen">
+      <section className="p-4 mx-5 lg:mx-14 min-h-screen">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
           {filteredItems?.map((tool) => (
             <a
